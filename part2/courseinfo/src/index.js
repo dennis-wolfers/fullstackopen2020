732139be5f5830c2204fa474fb8d2fb736969846
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-10-01 11:53:29 -0700

    implemented through 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Course from "./components/Course";
 
 const Courses = ({ courses }) => {
   return (
@@ -9,45 +10,6 @@ const Courses = ({ courses }) => {
         <Course key={course.id} course={course} />
       ))}
     </>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  );
-};
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>;
-};
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((acc, part) => {
-    return acc + part.exercises;
-  }, 0);
-  return <p>total of {total} exercises</p>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
   );
 };
 
