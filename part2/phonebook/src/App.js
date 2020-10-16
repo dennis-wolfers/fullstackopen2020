916b21229828a0b41556b268859fffd462b0dc9a
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-10-16 13:25:48 -0700

    phonebook implemented through 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,61 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = (props) => {
   return (
     <>
-    <form>
-      <div>
-        filter shown with<input value={props.filter} onChange={props.onChange()} />
-      </div>
-    </form>
+      <form>
+        <div>
+          filter shown with
+          <input value={props.filter} onChange={props.onChange()} />
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
 const AddEntry = (props) => {
   return (
     <form onSubmit={props.addName()}>
-        <div>
-          name: <input value={props.newName} onChange={props.handleNameChange()} />
-        </div>
-        <div>
-          phone: <input value={props.newPhone} onChange={props.handlePhoneChange()} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-  )
-}
+      <div>
+        name:{" "}
+        <input value={props.newName} onChange={props.handleNameChange()} />
+      </div>
+      <div>
+        phone:{" "}
+        <input value={props.newPhone} onChange={props.handlePhoneChange()} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
 
 const SingleEntry = (props) => {
   return (
     <>
-      <p key={props.person.name}>{props.person.name} {props.person.phone}</p>
+      <p key={props.person.key}>
+        {props.person.name} {props.person.number}
+      </p>
     </>
-  )
-}
+  );
+};
 
 const FilteredEntries = (props) => {
   return (
     <>
-    {props.persons.map(person => 
-      <SingleEntry person={person} />
-    )}
+      {props.persons.map((person) => (
+        <SingleEntry person={person} />
+      ))}
     </>
-  )
-}
+  );
+};
 
 const App = (props) => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas',      phone: '040-123456' },
-                                          { name: 'Ada Lovelace',     phone: '39-44-5323523' },
-                                          { name: 'Dan Abramov',      phone: '12-43-234345' },
-                                          { name: 'Mary Poppendieck', phone: '39-23-6423122' }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
-  const [newPhone, setNewPhone] = useState("")
-  const [filter, setFilter] = useState("")
+  const [newPhone, setNewPhone] = useState("");
+  const [filter, setFilter] = useState("");
 
-  const filteredPersons = persons.filter(person => person.name.toLowerCase().indexOf(filter) > -1)
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+
+  const filteredPersons = persons.filter(
+    (person) => person.name.toLowerCase().indexOf(filter) > -1
+  );
 
   const addName = (event) => {
     event.preventDefault();
@@ -65,33 +78,39 @@ const App = (props) => {
     } else {
       const nameObj = {
         name: newName,
-        phone: newPhone,
+        number: newPhone,
       };
 
       setPersons(persons.concat(nameObj));
       setNewName("");
-      setNewPhone("")
+      setNewPhone("");
     }
   };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
   const handlePhoneChange = (event) => {
-    setNewPhone(event.target.value)
-  }
+    setNewPhone(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter filter={filter} onChange={() => handleFilterChange} />
       <h2>Add a new</h2>
-      <AddEntry addName={() => addName} handleNameChange={() => handleNameChange} handlePhoneChange={() => handlePhoneChange} newName={newName} newPhone={newPhone} />
+      <AddEntry
+        addName={() => addName}
+        handleNameChange={() => handleNameChange}
+        handlePhoneChange={() => handlePhoneChange}
+        newName={newName}
+        newPhone={newPhone}
+      />
       <h2>Numbers</h2>
       <FilteredEntries persons={filteredPersons} />
     </div>
