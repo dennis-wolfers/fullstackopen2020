52c7ada2f6a773ab51300ad09dea6e7062b1bf6c
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-08-08 11:44:10 -0700

    5.9 implemented

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -114,7 +114,7 @@ const App = () => {
         <button onClick={handleLogout}>logout</button>
       </p>
       {blogForm()}
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} user={user} />
       )}
     </div>
